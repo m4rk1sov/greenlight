6f4ff3be5b7c485d25e4b6d85cee8403b73f11bc
m4rk1sov <134860120+m4rk1sov@users.noreply.github.com>
2024-04-22 16:10:19 +0500

    16th chapter end

--- internal/data/tokens.go
+++ internal/data/tokens.go
@@ -13,7 +13,8 @@ import (
 // Define constants for the token scope. For now we just define the scope "activation"
 // but we'll add additional scopes later in the book.
 const (
-	ScopeActivation = "activation"
+	ScopeActivation     = "activation"
+	ScopeAuthentication = "authentication" // Include a new authentication scope.
 )
 
 // never use math/rand for cryptographic, unless you need speed in certain scenarios
@@ -22,11 +23,11 @@ const (
 // plaintext and hashed versions of the token, associated user ID, expiry time and
 // scope.
 type Token struct {
-	Plaintext string
-	Hash      []byte
-	UserID    int64
-	Expiry    time.Time
-	Scope     string
+	Plaintext string    `json:"token"`
+	Hash      []byte    `json:"-"`
+	UserID    int64     `json:"-"`
+	Expiry    time.Time `json:"expiry"`
+	Scope     string    `json:"-"`
 }
 
 func generateToken(userID int64, ttl time.Duration, scope string) (*Token, error) {
